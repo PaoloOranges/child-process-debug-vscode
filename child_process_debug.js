6ddfe770df4b304cfd3c583be38d93517d1c1fd7
Paolo Oranges <paolo.oranges@gmail.com>
2018-09-04 20:55:51 +0200

    add mocha test main config. add param for child_proc_debug

--- child_process_debug.js
+++ child_process_debug.js
@@ -1,5 +1,12 @@
 const child_process = require('child_process');
 
 exports.fork = function fork(modulePath /* , args, options */) {
-    return child_process.fork(modulePath);
+
+    let args = {};
+    if(arguments.length > 1)
+    {
+        args = arguments[1];
+    }
+    
+    return child_process.fork(modulePath, args);
 }
