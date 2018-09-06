8c4b14c69cc3dd0f5c64022c1950096d90c14f1a
Paolo Oranges <paolo.oranges@gmail.com>
2018-09-06 14:56:38 +0200

    implement port shifting

--- child_process_debug.js
+++ child_process_debug.js
@@ -5,8 +5,15 @@ exports.fork = function fork(modulePath /* , args, options */) {
     let args = {};
     if(arguments.length > 1)
     {
+        const inspectParamIndex = process.execArgv.findIndex(arg => { return arg.includes("--inspect-brk"); });
+        if(inspectParamIndex != -1)
+        {
+            const newDebugPort = process.debugPort + 1;
+            process.execArgv[inspectParamIndex] = process.execArgv[inspectParamIndex].replace(/(--inspect-brk=)(\d*)/, "$1" + newDebugPort);
+        }
+
         args = arguments[1];
-    }
+    }    
     
     return child_process.fork(modulePath, args);
 }
