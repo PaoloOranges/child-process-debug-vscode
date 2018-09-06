552e0759c7b9215d056285cfea78abfbe61bd40b
Paolo Oranges <paolo.oranges@gmail.com>
2018-09-06 18:00:29 +0200

    fix implementation as per new tests

--- child_process_debug.js
+++ child_process_debug.js
@@ -1,17 +1,23 @@
 const child_process = require('child_process');
+let lastUsedDebugPort = 0;
 
 exports.fork = function fork(modulePath /* , args, options */) {
 
     let args = {};
+    if(lastUsedDebugPort == 0)
+    {
+        lastUsedDebugPort = process.debugPort;
+    }
+
+    const inspectParamIndex = process.execArgv.findIndex(arg => { return arg.includes("--inspect-brk"); });
+    if(inspectParamIndex != -1)
+    {
+        lastUsedDebugPort = lastUsedDebugPort + 1;
+        process.execArgv[inspectParamIndex] = process.execArgv[inspectParamIndex].replace(/(--inspect-brk=)(\d*)/, "$1" + lastUsedDebugPort);
+    }
+
     if(arguments.length > 1)
     {
-        const inspectParamIndex = process.execArgv.findIndex(arg => { return arg.includes("--inspect-brk"); });
-        if(inspectParamIndex != -1)
-        {
-            const newDebugPort = process.debugPort + 1;
-            process.execArgv[inspectParamIndex] = process.execArgv[inspectParamIndex].replace(/(--inspect-brk=)(\d*)/, "$1" + newDebugPort);
-        }
-
         args = arguments[1];
     }    
     
